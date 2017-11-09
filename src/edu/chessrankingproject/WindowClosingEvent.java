83f468ab6e1fc1420e1bce5ea835e18b9a8a6148
Tlowe <tmlow3@gmail.com>
2017-11-08 23:04:44 -0600

    new event type

---
+++ src/edu/chessrankingproject/WindowClosingEvent.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chessrankingproject;
+
+import java.awt.event.WindowEvent;
+
+
+/**
+ *
+ * @author Travis Lowe
+ */
+public class WindowClosingEvent extends java.util.EventObject {
+
+    public WindowClosingEvent(Object source) {
+        super(source);
+    }
+    
+}
